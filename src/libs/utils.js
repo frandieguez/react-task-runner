6d2e5e83d500079779a872d34298beecc0222fd6
Fran Dieguez <fran.dieguez@mabishu.com>
2019-03-10 12:48:24 +0100

    Add utils function with getRandomNumber and sleep functions

---
+++ src/libs/utils.js
@@ -0,0 +1,12 @@
+/**
+ * Returns a random number
+ * @param Int min - The minimum number accepted
+ * @param Int max - the maximum number accepted
+ *
+ * From: https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+ */
+export const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+export const sleep = m => new Promise(r => setTimeout(r, m))
