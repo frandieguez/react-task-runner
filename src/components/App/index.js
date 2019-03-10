c6ec0ca3d3429afe93841fd77094d52ead0bde32
Fran Dieguez <fran.dieguez@mabishu.com>
2019-03-10 10:37:28 +0100

    Add main styles
    Header and content wrapper are now coloured and displayed with taste ;)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,16 +5,22 @@ class App extends Component {
   render() {
     return (
       <div className={appStyles.App}>
-        <header className={appStyles.AppHeader}>
-          <a
-            className={appStyles.AppLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className={appStyles.header}>
+          <h1 className={appStyles.headerTitle}>Task runner</h1>
+
+          <div className={appStyles.copyright}>
+            &copy; 2019 Web application coded by {` `}
+            <a href="http://frandieguez.com/"
+              title="Go to website"
+              target="_blank" rel="noopener noreferrer">
+              Fran Dieguez
+            </a> {` `} using React.
+          </div>
         </header>
+
+        <section className={appStyles.runnerWrapper}>
+
+        </section>
       </div>
     );
   }
