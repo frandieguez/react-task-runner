cc3e1ad2868d96e3be6d189e9988e215bf60c5eb
Fran Dieguez <fran.dieguez@mabishu.com>
2019-03-10 10:27:53 +0100

    Improve App component
    Use CSS modules to style the app, clean the boilerplate code and
    minor fixes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import appStyles from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+      <div className={appStyles.App}>
+        <header className={appStyles.AppHeader}>
           <a
-            className="App-link"
+            className={appStyles.AppLink}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
