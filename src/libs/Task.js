c3dcfc8951787d7da20a43e7d46af9b14a9d56ca
Fran Dieguez <fran.dieguez@mabishu.com>
2019-03-10 13:02:04 +0100

    Ad function run that executes the tasks during a random number of seconds

--- src/libs/Task.js
+++ src/libs/Task.js
@@ -1,4 +1,9 @@
 import PropTypes from 'prop-types'
+import { getRandomNumber, sleep } from '../libs/utils';
+
+const MIN_SECONDS = 2;
+const MAX_SECONDS = 10;
+const MILISECONDS_IN_A_SEC = 1000;
 
 /** Class representing a task. */
 class Task {
@@ -8,11 +13,23 @@ class Task {
    * @param {string} name - The name of the task.
    */
   constructor(name) {
-    this.key    = Date.now();
+    this.key    = Math.random();
     this.name   = name;
     this.status = 'PENDING';
   }
+
+  async run() {
+    this.ttl =
+      getRandomNumber(MIN_SECONDS, MAX_SECONDS)
+      * MILISECONDS_IN_A_SEC;
+
+    // Complete the task after the ttl time.
+    await sleep(this.ttl);
+
+    return (Math.round(Math.random()) === 0) ? 'FAILED' : 'DONE';
+  }
 }
+
 
 Task.propTypes = {
   key: PropTypes.number.isRequired,
